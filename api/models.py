00ada237bb642134792ce590564e986100ac88fc
omarfaruk-k <ofkhan30@gmail.com>
2024-04-02 22:43:14 +0600

    initial commit

---
+++ api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    name =  models.CharField(max_length=200, null=False, blank=False)
+    catagory = models.CharField(max_length=180, null=False, blank=False)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    description = models.TextField()
+    stars = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name 
